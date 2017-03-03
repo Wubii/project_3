f562d002d6fc7ebe71966e1c211eb9008e90d1d7
BATTISTA Marion <battista.marion@gmail;com>
2017-03-03 19:19:52 +0100

    update

--- web/index.php
+++ web/index.php
@@ -4,179 +4,204 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . "/../autoload.php";
 
-$requestUrl = $_SERVER['REQUEST_URI'];
-
-switch($requestUrl)
-{
-	/* PAGE D'ACCUEIL--------------------------------------------------*/
-
-	case "/" :
-		
-		$controller = new MyController();
-
-		$controller->testAction();
-		break;
-
-
-	/* PAGE TEST ------------------------------------------------------*/
-
-	case "/test" :
+$routes = array(
+	new Route("/"              , "MyController::testAction"                 ),
+	new Route("/test"          , "TestController::testAction"               ),
 	
-		$controller = new TestController();
-		$controller->testAction();
+	new Route("/article"       , "ArticleController::articleListAction"     ),
+	new Route("/article/add"   , "ArticleController::articleAddAction"       , RequestMethodInterface::METHOD_GET ),
+	new Route("/article/add"   , "ArticleController::articleAddSubmitAction" , RequestMethodInterface::METHOD_POST),
+	new Route("/article/edit"  , "ArticleController::articleEditAction"      , RequestMethodInterface::METHOD_GET ),
+	new Route("/article/edit"  , "ArticleController::articleEditSubmitAction", RequestMethodInterface::METHOD_POST),
+	new Route("/article/delete", "ArticleController::articleDeleteAction"   ),
 
-		break;
+
+	new Route("/comment"       , "CommentController::commentListAction"     ),
+	new Route("/comment/add"   , "CommentController::commentAddAction"       , RequestMethodInterface::METHOD_GET ),
+	new Route("/comment/add"   , "CommentController::commentAddSubmitAction" , RequestMethodInterface::METHOD_POST),
+	new Route("/comment/edit"  , "CommentController::commentEditAction"      , RequestMethodInterface::METHOD_GET ),
+	new Route("/comment/edit"  , "CommentController::commentEditSubmitAction", RequestMethodInterface::METHOD_POST),
+	new Route("/comment/delete", "CommentController::commentDeleteAction"   ),
+);
+
+$router = new Router($routes);
+
+$router->matchCurrentRequest();
+
+
+// $requestUrl = $_SERVER['REQUEST_URI'];
+
+// switch($requestUrl)
+// {
+// 	/* PAGE D'ACCUEIL--------------------------------------------------*/
+
+// 	case "/" :
+		
+// 		$controller = new MyController();
+
+// 		$controller->testAction();
+// 		break;
+
+
+// 	/* PAGE TEST ------------------------------------------------------*/
+
+// 	case "/test" :
+	
+// 		$controller = new TestController();
+// 		$controller->testAction();
+
+// 		break;
 		
 
-	/* PAGE TEST AUTOLOAD ----------------------------------------------*/
+// 	/* PAGE TEST AUTOLOAD ----------------------------------------------*/
 
-	case "/autoload" :
+// 	case "/autoload" :
 
-		$controller = new Controller1();
+// 		$controller = new Controller1();
 
-		$controller->toStringAction();
+// 		$controller->toStringAction();
 
-		break;
+// 		break;
 		
 
-	/* CRÉATION ET MODIFICATION D'UN ARTICLE ---------------------------*/
+// 	/* CRÉATION ET MODIFICATION D'UN ARTICLE ---------------------------*/
 
-	case "/article/new" :
+// 	case "/article/new" :
 
-	    $article1 = new Article();
-	    $article1->setTitle("Un nouvel article");
-	    $article1->setContent("contenu 0");
-	    $article1->persist();
-	    echo "L'article a bien été ajouté </br>";
+// 	    $article1 = new Article();
+// 	    $article1->setTitle("Un nouvel article");
+// 	    $article1->setContent("contenu 0");
+// 	    $article1->persist();
+// 	    echo "L'article a bien été ajouté </br>";
 
-	    $article1->setTitle("Un nouvel article");
-	    $article1->setContent("contenu 1");
-	    $article1->persist();
-	    echo "L'article a bien été modifié </br>";
+// 	    $article1->setTitle("Un nouvel article");
+// 	    $article1->setContent("contenu 1");
+// 	    $article1->persist();
+// 	    echo "L'article a bien été modifié </br>";
 
-	    $article2 = new Article();
-	    $article2->setTitle("Un deuxieme nouvel article");
-	    $article2->setContent("deuxieme contenu");
-	    $article2->persist();
-	    echo "Le deuxième article a bien été créé </br>";
+// 	    $article2 = new Article();
+// 	    $article2->setTitle("Un deuxieme nouvel article");
+// 	    $article2->setContent("deuxieme contenu");
+// 	    $article2->persist();
+// 	    echo "Le deuxième article a bien été créé </br>";
 
-		break;
+// 		break;
 		
 
-	/* AFFICHE TOUS LES ARTICLES --------------------------------------*/
+// 	/* AFFICHE TOUS LES ARTICLES --------------------------------------*/
 
-	case "/article/list" :
+// 	case "/article/list" :
 
-	    $articles = Article::findAll();
+// 	    $articles = Article::findAll();
 
-    	foreach ($articles as $article) 
-    	{
-        	echo "</br> Id : " . $article->getId() . "</br>" . $article->getTitle() . " Author : " . $article->getAuthor() . " Écrit le : " . $article->getDate()->format('Y-m-d H:i:s') . "</br>" . $article->getContent(). "</br>";
-    	}
+//     	foreach ($articles as $article) 
+//     	{
+//         	echo "</br> Id : " . $article->getId() . "</br>" . $article->getTitle() . " Author : " . $article->getAuthor() . " Écrit le : " . $article->getDate()->format('Y-m-d H:i:s') . "</br>" . $article->getContent(). "</br>";
+//     	}
 
-		break;
+// 		break;
 		
 
-	/* AFFICHE UN ARTICLE----------------------------------------------*/
+// 	/* AFFICHE UN ARTICLE----------------------------------------------*/
 
-	case "/article/find_by_id" :
+// 	case "/article/find_by_id" :
     	
-    	$article = Article::findById(6);
+//     	$article = Article::findById(6);
 
-        echo $article->getTitle() . " Author : " . $article->getAuthor() . " Écrit le : " . $article->getDate()->format('Y-m-d H:i:s') . "</br>" . $article->getContent();
+//         echo $article->getTitle() . " Author : " . $article->getAuthor() . " Écrit le : " . $article->getDate()->format('Y-m-d H:i:s') . "</br>" . $article->getContent();
 
-    	break;
+//     	break;
 		
 
-	/* SUPPRIME UN ARTICLE---------------------------------------------*/
+// 	/* SUPPRIME UN ARTICLE---------------------------------------------*/
 
-	case "/article/remove" :
+// 	case "/article/remove" :
     	
-    	$article = Article::findById(3);
+//     	$article = Article::findById(3);
 
-    	if($article != null)
-    	{
-        	echo $article->getTitle() . "</br>";
+//     	if($article != null)
+//     	{
+//         	echo $article->getTitle() . "</br>";
 
-        	$result = $article->remove();
-    	}
+//         	$result = $article->remove();
+//     	}
 
-		break;
+// 		break;
 		
 
-	/* AJOUTE UN COMMENTAIRE ------------------------------------------*/
+// 	/* AJOUTE UN COMMENTAIRE ------------------------------------------*/
 
-	case "/comment/new" :
+// 	case "/comment/new" :
 
-	    $comment1 = new Comment();
-	    $comment1->setTitle("Un nouvel commentaire");
-	    $comment1->setContent("Contenu du premier commentaire");
-	    $comment1->setPseudo("Wubiii");
-	    $comment1->persist();
-	    echo "Le commentaire a bien été ajouté </br>";
+// 	    $comment1 = new Comment();
+// 	    $comment1->setTitle("Un nouveau commentaire");
+// 	    $comment1->setContent("Contenu du premier commentaire");
+// 	    $comment1->setPseudo("Wubiii");
+// 	    $comment1->persist();
+// 	    echo "Le commentaire a bien été ajouté </br>";
 
-	    $comment1->setTitle("Un nouvel commentaire");
-	    $comment1->setContent("Contenu modifié du premier commentaire");
-	    $comment1->setPseudo("Wubiii");
-	    $comment1->persist();
-	    echo "Le commentaire a bien été modifié </br>";
+// 	    $comment1->setTitle("Un nouvel commentaire");
+// 	    $comment1->setContent("Contenu modifié du premier commentaire");
+// 	    $comment1->setPseudo("Wubiii");
+// 	    $comment1->persist();
+// 	    echo "Le commentaire a bien été modifié </br>";
 
-	    $comment2 = new Comment();
-	    $comment2->setTitle("Un deuxieme commentaire");
-	    $comment2->setContent("Contenu du deuxième commentaire");
-	    $comment2->setPseudo("Comos");
-	    $comment2->persist();
-	    echo "Le deuxième commentaire a bien été créé </br>";
+// 	    $comment2 = new Comment();
+// 	    $comment2->setTitle("Un deuxieme commentaire");
+// 	    $comment2->setContent("Contenu du deuxième commentaire");
+// 	    $comment2->setPseudo("Comos");
+// 	    $comment2->persist();
+// 	    echo "Le deuxième commentaire a bien été créé </br>";
 
-		break;
+// 		break;
 		
 
-	/* LISTE TOUS LES COMMENTAIRES ------------------------------------*/
+// 	/* LISTE TOUS LES COMMENTAIRES ------------------------------------*/
 
-	case "/comment/list" :
+// 	case "/comment/list" :
 
-	    $comments = Comment::findAll();
+// 	    $comments = Comment::findAll();
 
-    	foreach ($comments as $comment) 
-    	{
-        	echo "</br> Id : " . $comment->getId() . "</br>" . $comment->getTitle() . " Author : " . $comment->getPseudo() . " Écrit le : " . $comment->getDate()->format('Y-m-d H:i:s') . "</br>" . $comment->getContent(). "</br>";
-    	}
+//     	foreach ($comments as $comment) 
+//     	{
+//         	echo "</br> Id : " . $comment->getId() . "</br>" . $comment->getTitle() . " Author : " . $comment->getPseudo() . " Écrit le : " . $comment->getDate()->format('Y-m-d H:i:s') . "</br>" . $comment->getContent(). "</br>";
+//     	}
 
-		break;
+// 		break;
 		
 
-	/* AFFICHE UN COMMENTAIRE -----------------------------------------*/
+// 	/* AFFICHE UN COMMENTAIRE -----------------------------------------*/
 
-	case "/comment/find_by_id" :
+// 	case "/comment/find_by_id" :
     	
-    	$comment = Comment::findById(6);
+//     	$comment = Comment::findById(6);
 
-        echo $comment->getTitle() . " Pseudo : " . $comment->getPseudo() . " Écrit le : " . $comment->getDate()->format('Y-m-d H:i:s') . "</br>" . $comment->getContent();
+//         echo $comment->getTitle() . " Pseudo : " . $comment->getPseudo() . " Écrit le : " . $comment->getDate()->format('Y-m-d H:i:s') . "</br>" . $comment->getContent();
 
-    	break;
+//     	break;
 		
 
-	/* SUPRRIME UN COMMENTAIRE ----------------------------------------*/
+// 	/* SUPRRIME UN COMMENTAIRE ----------------------------------------*/
 
-	case "/comment/remove" :
+// 	case "/comment/remove" :
     	
-    	$comment = Comment::findById(4);
+//     	$comment = Comment::findById(4);
 
-    	if($comment != null)
-    	{
-        	$result = $comment->remove();
+//     	if($comment != null)
+//     	{
+//         	$result = $comment->remove();
 
-        	echo "Le commentaire : \'". $comment->getTitle() . "\' a été supprimé</br>";
-    	}
+//         	echo "Le commentaire : \'". $comment->getTitle() . "\' a été supprimé</br>";
+//     	}
 
-		break;
+// 		break;
 
-	default :
+// 	default :
 		
-		echo "Erreur : URL non valide ! ";
+// 		echo "Erreur : URL non valide ! ";
 
-		break;
-}
+// 		break;
+//}
 
 
 
