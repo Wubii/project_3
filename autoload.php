c4d1402e5b1ea724c62379b322253f9ac4bf8d32
BATTISTA Marion <battista.marion@gmail;com>
2017-03-24 16:36:21 +0100

    custom view

--- autoload.php
+++ autoload.php
@@ -30,6 +30,18 @@ spl_autoload_register(function ($class)
 	} 
 });
 
+spl_autoload_register(function ($class)
+{
+	$fileName = __DIR__ . "/lib/Session/" . $class . ".php";
+
+	if(file_exists($fileName)) 
+	{
+		require_once($fileName);
+	} 
+});
+
 require_once(__DIR__ . "/lib/Twig/Autoloader.php");
 
 Twig_Autoloader::register();
+
+require_once __DIR__ . "/lib/PHPMailer/PHPMailerAutoload.php";
